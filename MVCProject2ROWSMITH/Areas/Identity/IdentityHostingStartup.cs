ea84b2acd96b5cb031dca74d98a4c4efa8c2ff76
Ryan Smith <ryan450.rs@gmail.com>
2020-11-19 16:34:03 +0200

    Intial commit

---
+++ MVCProject2ROWSMITH/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MVCProject2ROWSMITH.Data;
+
+[assembly: HostingStartup(typeof(MVCProject2ROWSMITH.Areas.Identity.IdentityHostingStartup))]
+namespace MVCProject2ROWSMITH.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
